961968bd828587a70c31c30aa246aefe5b3ddc81
MrMakako <diegofdiaz1717@gmail.com>
2022-03-11 14:01:30 -0600

    Primer commit

---
+++ src/carreraslab2/pkg8/DirManager.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carreraslab2.pkg8;
+
+import java.io.File;
+
+/**
+ *
+ * @author Diego Carcamo
+ */
+public class DirManager {
+    
+    
+    File Carpter;
+
+    public DirManager() {
+    }
+    
+    
+    
+}
